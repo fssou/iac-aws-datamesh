5c7dd57bd9d99f0de00421c8a01dbc89ede5c933
Franclin Sousa <10904895+fssou@users.noreply.github.com>
2023-10-15 06:47:01 -0300

    fix: ajusta saida de bucket de modulos

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 
 module "data_catalog" {
   source = "./modules/data_catalog"
-  bucket_name_sor = module.data_stores.bucket_sor.name
-  bucket_name_sot = module.data_stores.bucket_sot.name
-  bucket_name_spec = module.data_stores.bucket_spec.name
+  bucket_name_sor = module.data_stores.bucket_sor.bucket
+  bucket_name_sot = module.data_stores.bucket_sot.bucket
+  bucket_name_spec = module.data_stores.bucket_spec.bucket
 }
 
 module "data_stores" {
--- modules/data_stores/outputs.tf
+++ modules/data_stores/outputs.tf
@@ -1,16 +1,16 @@
 
 output "bucket_raw" {
-  value = module.raw
+  value = module.raw.bucket
 }
 
 output "bucket_sor" {
-  value = module.sor
+  value = module.sor.bucket
 }
 
 output "bucket_sot" {
-  value = module.sot
+  value = module.sot.bucket
 }
 
 output "bucket_spec" {
-  value = module.spec
+  value = module.spec.bucket
 }
