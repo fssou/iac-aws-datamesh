ead95bb4668e276d34df1272e8a88f5c6dcb5f73
Franclin Sousa <10904895+fssou@users.noreply.github.com>
2023-10-15 08:30:06 -0300

    chore: ajusta uso de acl em bucket

--- modules/data_stores/modules/raw/main.tf
+++ modules/data_stores/modules/raw/main.tf
@@ -1,5 +1,4 @@
 
-# Create S3 bucket for raw data
 resource "aws_s3_bucket" "data" {
   bucket = "raw.data.francl.in"
   tags = {
@@ -9,8 +8,47 @@ resource "aws_s3_bucket" "data" {
   }
 }
 
-# Create S3 bucket acl private
-resource "aws_s3_bucket_acl" "data" {
+
+resource "aws_s3_bucket_policy" "data" {
   bucket = aws_s3_bucket.data.id
-  acl    = "private"
+  policy = data.aws_iam_policy_document.data.json
+}
+
+data "aws_iam_policy_document" "data" {
+  version = "2012-10-17"
+  statement {
+    sid = "DenyAll"
+    principals {
+      type = "*"
+      identifiers = [
+        "*"
+      ]
+    }
+    actions = [
+      "s3:*"
+    ]
+    effect = "Deny"
+    resources = [
+      aws_s3_bucket.data.arn,
+      "${aws_s3_bucket.data.arn}/*",
+    ]
+  }
+  statement {
+    sid = "AllowAllInternal"
+    principals {
+      type = "AWS"
+      identifiers = [
+        "arn:aws:iam::${data.aws_caller_identity.main.account_id}:root"
+      ]
+    }
+    actions = [
+      "s3:*",
+    ]
+    effect = "Allow"
+    resources = [
+      aws_s3_bucket.data.arn,
+      "${aws_s3_bucket.data.arn}/*",
+    ]
+  }
+
 }
