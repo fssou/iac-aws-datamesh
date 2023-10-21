170e61b8c59e1c684d7cef08fd317955a65925c4
Franclin Sousa <10904895+fssou@users.noreply.github.com>
2023-10-21 13:06:02 -0300

    feat: campos particionados novos nomes

--- modules/data_catalog/db.data_world.0001.tf
+++ modules/data_catalog/db.data_world.0001.tf
@@ -9,25 +9,24 @@ resource "aws_glue_catalog_table" "tb_covid_19_data_resource_hub_covid_19_case_c
   catalog_id    = data.aws_caller_identity.main.account_id
   table_type    = "external_table"
   description   = "This table contains COVID-19 case counts from the COVID-19 Data Resource Hub. The COVID-19 Data Resource Hub is a collection of the best available data for countries, states and counties. The data is updated daily and primarily sourced from public data repositories such as Johns Hopkins University, the CDC, WHO, and a few others. The data is collected, cleaned, and made available in a single place for analysis. The data is available in CSV and JSON formats. The data is also available via an API. The data is available for free and is licensed under the Creative Commons Attribution-ShareAlike 4.0 International license. The data is updated daily. The data is available for download from the COVID-19 Data Resource Hub website. The data is also available via an API. The data is available for free and is licensed under the Creative Commons Attribution-ShareAlike 4.0 International license. The data is updated daily."
-
   parameters = {
     "external"            = "true"
     "parquet.compression" = "snappy"
   }
   partition_keys {
-    name    = "report_date"
+    name    = "part_report_date"
     type    = "date"
-    comment = "Date of report in YYYY-MM-DD format"
+    comment = "Partition of Date of report in YYYY-MM-DD format"
   }
   partition_keys {
-    name    = "country_alpha_2_code"
+    name    = "part_country_alpha_2_code"
     type    = "string"
-    comment = "Country alpha-2 code"
+    comment = "Partition of Country alpha-2 code"
   }
   partition_keys {
-    name    = "continent_name"
+    name    = "part_continent_name"
     type    = "string"
-    comment = "Continent name"
+    comment = "Partition of Continent name"
   }
   storage_descriptor {
     location      = "s3://${var.bucket_name_sor}/${local.data_world_0001_db_name}/${local.data_world_0001_tb_name}"
@@ -39,6 +38,21 @@ resource "aws_glue_catalog_table" "tb_covid_19_data_resource_hub_covid_19_case_c
       parameters = {
         "serialization.format" = 1
       }
+    }
+    columns {
+      name    = "report_date"
+      type    = "date"
+      comment = "Date of report in YYYY-MM-DD format"
+    }
+    columns {
+      name    = "country_alpha_2_code"
+      type    = "string"
+      comment = "Country alpha-2 code"
+    }
+    columns {
+      name    = "continent_name"
+      type    = "string"
+      comment = "Continent name"
     }
     columns {
       name    = "people_positive_cases_count"
